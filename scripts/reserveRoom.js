265f9678c613988a40fa484152d86d75914fa6cd
Eli0uce <eliouce.gaming@outlook.fr>
2023-01-25 13:13:31 +1100

    updateMain

--- scripts/reserveRoom.js
+++ scripts/reserveRoom.js
@@ -5,13 +5,15 @@ function showReserveRoomBox() {
       '<input id="id" type="hidden">' +
       '<input id="name" class="swal2-input" placeholder="Prénom">' +
       '<input id="last_name" class="swal2-input" placeholder="Nom">' +
-      '<input type="datetime" id="date_debut" class="swal2-input" placeholder="Date de début">' +
-      '<input type="datetime" id="date_fin" class="swal2-input" placeholder="Date de fin">' +
+      '<label for="date_debut">Date de début</label>' +
+      '<input type="datetime-local" id="date_debut" class="swal2-input">' +
+      '<label for="date_fin">Date de fin</label>' +
+      '<input type="datetime-local" id="date_fin" class="swal2-input">' +
       '<select id="rooms"><option value="onechambre">1 Chambre</option></select>' +
       '<select id="number"><option value="one">1 personne</option></select>',
     focusConfirm: false,
-    preConfirm: () => {
-      reserveRoomAdd();
-    },
+    // preConfirm: () => {
+    //   reserveRoomAdd();
+    // },
   });
 }
