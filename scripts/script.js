7c1ef82ad8ac2e054f9470a4f467de736de7630e
Eli0uce <eliouce.gaming@outlook.fr>
2023-01-25 17:07:09 +1100

    Services

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,12 @@ function showReserveRoomBox() {
     // },
   });
 }
+
+// Get the button:
+let mybutton = document.getElementById("btt");
+
+// Scroll to the top of the page on click
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
